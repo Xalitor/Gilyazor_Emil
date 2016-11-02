2e1282c27ea57f6c237697270a702562ee7715d4
Xalitor <figov14@bk.ru>
2016-11-02 10:24:53 -0700

    Addedfiles

--- lesson_04/Ex_04.py
+++ lesson_04/Ex_04.py
@@ -1,2 +1,9 @@
-def format(L, H, W, volume):
-    return()
+def format(L, H, W):
+        return((L*H*W)/1728)
+
+L=float(input("Please, enter your subwoofer box's lenght "))
+H=float(input("Please, enter your subwoofer box's height "))
+W=float(input("Please, enter your subwoofer box's width "))
+
+print("Your volume in cubic inches is ", L*H*W, ".")
+print("Your volume in cubic feet is ", (L*H*W)/1728, "." )
