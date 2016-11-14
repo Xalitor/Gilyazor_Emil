7aef8bdbe4784d7b324bc5b7a77865f00a6f88fe
Xalitor <figov14@bk.ru>
2016-11-14 10:27:59 -0800

    addedfiles

---
+++ lesson_05/Exercise_01_PyLab_05_1.py
@@ -0,0 +1,24 @@
+math=input("What is your Math's grade? ")
+eng=input("What is your English's grade? ")
+physics=input("What is your Physics's grade? ")
+gym=input("What is your PE's grade? ")
+art=input("What is your Sculpture's grade? ")
+history=input("What is your history's grade? ")
+comp=input("What is your Computer Science's grade? ")
+
+def calcPoints():
+    return((math+eng+physics+gym+art+history+comp)/7)
+    if grade=="a"
+        return 4.0
+    elif grade
+        print("Your GPA is B")
+    elif grade
+        print("Your GPA is C")
+    elif grade
+        print("Your GPA is D")
+    else:
+        print("Your GPA is F")
+
+
+calcPoints()
+
--- lesson_05/Exercise_02.py
+++ lesson_05/Exercise_02.py
@@ -1,7 +1,19 @@
 global discount, subtotal, total
-dicount=0
-subtotal=0
-total=0
+subtotal=0;
+discount=0;
+total=0;
+def format (item, price):
+    print("{:<15}.........${:10.2f}".format(item, price))
+
+def discount():
+    global subtotal, discount, total
+    if subtotal >= 2000:
+        discount=subtotal * 0.15
+    if subtotal <= 2000:
+        discount=0
+
+
+
 item1 = input("Please, enter first item ")
 price1 = float(input("Please, enter first price "))
 item2 = input("Please, enter second item ")
@@ -11,27 +23,19 @@ price3 = float(input("Please, enter third price "))
 item4 = input("Please, enter fourth item ")
 price4 = float(input("Please, enter fourth price "))
 
-
-
-
 subtotal = price1+price2+price3+price4
-
-tax = subtotal*0.08
-
-def discount():
-    if subtotal>=2000:
-        discount=subtotal*0.15
-    if subtotal<=2000:
-        discount=subtotal*0
 discount()
-
-total = subtotal - discount + tax
-
+tax = subtotal*0.08
+total = subtotal-discount+tax
 
 print("<<<<<<<<<<< Receipt >>>>>>>>>>>>>>")
-print("{:.2f} = , {:.2f} = {:.2f}, {:.2f} = {:.2f}, {:.2f} = {:.2f}".format(item1, price1, item2, price2, item3, price3, item4, price4))
-print("Subtotal is {:.2f}".format(subtotal))
-print("Discount is {:.2f}".format(discount))
-print("____________________________________")
-print("Total is {:.2f}".format(total))
+format(item1, price1)
+format(item2, price2)
+format(item3, price3)
+format(item4, price4)
+
+format("Subtotal", subtotal)
+format("Discount", discount)
+format("Total", total)
+print("_______________________________")
 print("Thank you!")
