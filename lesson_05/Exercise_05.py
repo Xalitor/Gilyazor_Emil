e626b61070509ffc651d7bbfc88b2016cfcf8099
Xalitor <figov14@bk.ru>
2016-11-18 10:23:51 -0800

    added files

---
+++ lesson_05/Exercise_05.py
@@ -0,0 +1,24 @@
+print("We are going to adventure")
+a = int(input("Where would you want to go?"+
+          "\n 1. Volcano"+
+          "\n 2. Death Lands"))
+if a==1:
+    print("You are going to Volcano!")
+    b=int(input("You seeing a volcano monster! What do you want to do?"+
+              "\n 1. Hit him!"+
+              "\n 2. Run away throught the forest."))
+    if b==1:
+        print("You hit him, but he is very strong, and... You ran away throught the forest...")
+        c=int(input("You went to San Diego! Where would you want to go?"+
+                    "\n 1. Torrey Pines High School."+
+                    "\n 2. Costco."))
+        if c==1:
+            print("You became a teacher in Computer Science at Torrey Pines High School! Be happy!!!")
+        else:
+            print("You bought a new strong sword and went to Volcano then you killed a volcano monster! Be happy!!!")
+    else:
+        print("You went to ")
+  
+    
+            
+        
