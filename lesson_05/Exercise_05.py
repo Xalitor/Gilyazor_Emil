325736204f3e94ba3baebf699286ee148fffd808
Xalitor <figov14@bk.ru>
2016-11-28 10:24:57 -0800

    added files

--- lesson_05/Exercise_05.py
+++ lesson_05/Exercise_05.py
@@ -1,7 +1,7 @@
 print("We are going to adventure")
 a = int(input("Where would you want to go?"+
           "\n 1. Volcano"+
-          "\n 2. Death Lands"))
+          "\n 2. Dead Lands"))
 if a==1:
     print("You are going to Volcano!")
     b=int(input("You seeing a volcano monster! What do you want to do?"+
@@ -17,7 +17,44 @@ if a==1:
         else:
             print("You bought a new strong sword and went to Volcano then you killed a volcano monster! Be happy!!!")
     else:
-        print("You went to ")
+        print("You ran away throught the forest...")
+        d=int(input("You went to Dinosaur island! What do you want to do?"+
+                    "\n 1. Go to Tyrannosaur."+
+                    "\n 2. Go to Stegosaurs."))
+        if d==1:
+            print("You have been eaten...")
+        else:
+            print("You're riding on the stegosaur and living with them. Be happy!!!")
+else:
+    print("You went to Dead Lands!")
+    e=int(input("You are seeing Death's Castle and Empty Land."+
+                "\n 1. You want to go to Castle."+
+                "\n 2. You want to go to Empty Land."))
+    if e==1:
+        print("You met a Demon!")
+        f=int(input("What do you want to do?"+
+                    "\n 1. You want to meet Death."+
+                    "\n 2. You want to run away."))
+        if f==1:
+            print("Death took you with her! You are dead!")
+        else:
+            print("You tried to ran away, but Demon is very fast and took you in slavery! You are live and dead together.")
+    else:
+        print("You went to Empty Land!")
+        g=int(input("You are seeing a totem of power! What do you want to do?"+
+                    "n 1. You want to live."+
+                    "n 2. You want to die."))
+        if g==1:
+            print("Totem sent you back to your world!")
+        else:
+            print("Totem sent you to Empty Place... Nobody know about this place. You are alone and can't die.")
+
+
+
+
+            
+        
+
   
     
             
