cd1e40bffb3d49f6acd0ac6fb2d83cf9d0787196
Xalitor <figov14@bk.ru>
2017-01-04 10:24:04 -0800

    addedfiles

--- lesson_09/Ex_6.py
+++ lesson_09/Ex_6.py
@@ -1,17 +1,30 @@
 expression = input("Enter your mathematical expression ")
-equation=[]
-myList = equation.split(" ")
+equation=expression.split(" ")
 print(equation)
 i=0
 while i<len(equation):
     if i < len(equation) and (equation[i]=="/" or equation[i]=="*"):
         if equation[i]=="*":
-            equation[i]==int(equation[i-1])* int(equation[i+1])
+            equation[i]=int(equation[i-1])* int(equation[i+1])
         else:
-            equation[i] == int(equation[i-1])/int(equation[i+1])
-        equation.pop[i-1]
-        equation.pop[i]
-    equation[i+1]
+            equation[i] =int(equation[i-1])/int(equation[i+1])
+            print(equation)
+        equation.pop(i-1)
+        equation.pop(i)
+    else:
+        i+=1
+i=0
+while i < len(equation):
+    if i<len(equation) and (equation[i]=="+" or equation[i]=="-"):
+        if equation[i]=="+":
+            equation[i]=int(equation[i-1])+int(equation[i+1])
+        else:
+            equation[i]=int(equation[i-1])-int(equation[i+1])
+        equation.pop(i-1)
+        equation.pop(i)
+    else:
+        i+=1
+print(equation)
         
   
             
