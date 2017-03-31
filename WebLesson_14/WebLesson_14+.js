e6004dda2b97a958ce8caf8962a54d9ba3cf38eb
Xalitor <figov14@bk.ru>
2017-03-31 10:24:52 -0700

    addedfiles

--- WebLesson_14/WebLesson_14+.js
+++ WebLesson_14/WebLesson_14+.js
@@ -2,6 +2,7 @@ function validate() {
 	var x = document.forms.input.username.value;
 	var atPos = x.indexOf("@");
 	var dotPos = x.lastIndexOf(".");
+	var y = document.forms.input.password.value;
 	//we need... username@webaddress.extension
 	//if, the following...
 		//@ is not in the string
@@ -9,9 +10,13 @@ function validate() {
 		//there is no .com, .gov, or any other extension
 		//final dot is in the wrong place
 	
+	
 	if(atPos < 1 || dotPos < atPos+2 || dotPos+2 > x.length)
 		alert("This is not a valid email address!");
-		
+	else if(6 > y.length)
+		alert("Your password is so short!")
+	else if(6 > y.length || atPos < 1 || dotPos < atPos+2 || dotPos+2 > x.length)
+		alert("Both username and password are incorrect!")
 	else
 		alert("Success!")
 }
