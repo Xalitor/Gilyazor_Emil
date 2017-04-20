a2a6c692e24b724d466dd62d37eee18ce3e94a91
Xalitor <figov14@bk.ru>
2017-04-20 10:23:17 -0700

    addefiles

--- WebLesson_16/WebLab_16+.js
+++ WebLesson_16/WebLab_16+.js
@@ -6,7 +6,7 @@ function mouse()
 
 	
 	window.addEventListener("mousemove", icon, false);
-	pic.addEventListener("load", function() {canvas.drawImage(pic, 0, 0, 200, 200);}, false)
+
 	
 }
 
@@ -16,10 +16,11 @@ function icon(e) {
 	var yPos = e.clientY;
 	var pic = new Image();
 	pic.src = "http://thedesigninspiration.com/wp-content/uploads/2010/05/animals/006.jpg";
+	
 	canvas.fillRect(xPos-50, yPos-50, 100, 100);
 	
 	
 	
 }
-
+pic.addEventListener("load", function() {canvas.drawImage(pic, 0, 0, 200, 200);}, false);
 window.addEventListener("load", mouse, false);
